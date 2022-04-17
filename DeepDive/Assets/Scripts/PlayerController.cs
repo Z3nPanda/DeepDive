cbd7eae6df85650646526ac83d06723761630bcd
Ollie Goldstein <goldsteinollie@gmail.com>
2022-04-17 15:10:42 -0700

    Player Movement Script
    script added

---
+++ DeepDive/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeFowardSpeed, activestrafeSpeed, activeHoverSpeed;
+    private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration  = 2f;
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDist;
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenCenter.x = Screen.width * 0.5f;
+        screenCenter.y = Screen.height * 0.5f;
+
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        mouseDist.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDist.y = (lookInput.y - screenCenter.y) /  screenCenter.y;
+
+        mouseDist = Vector2.ClampMagnitude(mouseDist, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDist.y * lookRateSpeed * Time.deltaTime, mouseDist.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+        activeFowardSpeed = Mathf.Lerp(activeFowardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        activestrafeSpeed = Mathf.Lerp(activestrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
+        activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
+
+        transform.position += transform.forward * activeFowardSpeed * Time.deltaTime;
+        transform.position += transform.right * activestrafeSpeed * Time.deltaTime;
+        transform.position += transform.up * activeHoverSpeed * Time.deltaTime;
+    }
+}
