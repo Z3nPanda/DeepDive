7b16129c291f4d2aaa04b31be2d50349336d8e6f
Ollie Goldstein <goldsteinollie@gmail.com>
2022-04-17 15:28:28 -0700

    Update PlayerController.cs
    added comments to player movement script

--- DeepDive/Assets/Scripts/PlayerController.cs
+++ DeepDive/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// script derived from https://www.youtube.com/watch?v=J6QR4KzNeJU
+
 public class PlayerController : MonoBehaviour
 {
     public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
@@ -12,32 +14,44 @@ public class PlayerController : MonoBehaviour
     private float rollInput;
     public float rollSpeed = 90f, rollAcceleration = 3.5f;
 
+    // Start is called before the first frame update
     void Start()
     {
+        // adjusting screen size to player's computer screen sematics
         screenCenter.x = Screen.width * 0.5f;
         screenCenter.y = Screen.height * 0.5f;
 
+        // lock the mouse inside the game screen
         Cursor.lockState = CursorLockMode.Confined;
     }
 
+    // Update is called once per frame
     void Update()
     {
+        // set lookInput based on mouse position
         lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
 
+        // calculate the distance between the center of the screen and where the mouse
+        // is and adjust coordinates
         mouseDist.x = (lookInput.x - screenCenter.x) / screenCenter.y;
         mouseDist.y = (lookInput.y - screenCenter.y) /  screenCenter.y;
 
+        // set limits
         mouseDist = Vector2.ClampMagnitude(mouseDist, 1f);
 
+        // set roll input
         rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
 
+        // actually rotate the player
         transform.Rotate(-mouseDist.y * lookRateSpeed * Time.deltaTime, mouseDist.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
 
+        // set active speeds using base speeds and axises 
         activeFowardSpeed = Mathf.Lerp(activeFowardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
         activestrafeSpeed = Mathf.Lerp(activestrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
         activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
 
+        // move the player
         transform.position += transform.forward * activeFowardSpeed * Time.deltaTime;
         transform.position += transform.right * activestrafeSpeed * Time.deltaTime;
         transform.position += transform.up * activeHoverSpeed * Time.deltaTime;
