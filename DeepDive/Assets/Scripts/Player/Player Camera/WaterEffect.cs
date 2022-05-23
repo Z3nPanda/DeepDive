83e77e5e1b82f26464a2df061df6c9be94a80990
root <root@DESKTOP-NUBMVOA.localdomain>
2022-05-22 17:02:40 -0700

    Improved water environment with shaders and a new skybox

--- DeepDive/Assets/Scripts/Player/Player Camera/WaterEffect.cs
+++ DeepDive/Assets/Scripts/Player/Player Camera/WaterEffect.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class WaterEffect : MonoBehaviour
 {
-
-    void Start()
+    public Material effect_material;
+    void OnRenderImage(RenderTexture source, RenderTexture destination) 
     {
-        RenderSettings.fog = true;
-        RenderSettings.fogColor = new Color(0.13f, 0.62f, 0.91f, 0.25f);
-        RenderSettings.fogDensity = 0.005f; 
+        Graphics.Blit(source, destination, effect_material);
     }
 }
